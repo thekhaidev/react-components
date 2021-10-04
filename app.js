c092904855e28ecc95c89f2fe3b5c25b13049624
Khai Tran <kqtran917@yahoo.com>
2021-10-04 13:11:46 -0500

    Implemented interaction for crossing off item

--- app.js
+++ app.js
@@ -13,19 +13,40 @@ var App = () => (
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props); // This is same as React.component.call(this, props) in ES5
+
+      // `state` is just an object literal
+      // current state of item is not bought
+      this.state = {
+        bought: false
+      };
+  }
+
+    // When a list item is clicked, we will toggle the `bought`
+  // boolean, and our component's `render` method will run again
+  onListItemClick() {
+    this.setState({
+      bought: !this.state.bought
+    });
   }
 
   // Class must have render method
 // Stateless functional components are basically just this
 render() {
-  // Must use return due to curly brackets
-  // Using this.props instead of props
-  return (
-    <li>{this.props.list}</li>
-  );
+    // Making the style conditional on our `state` lets us
+    // update it based on user interactions.
+    var style = {
+      textDecoration: this.state.bought ? 'line-through' : 'none'
+    };
+
+    // You can pass inline styles using React's `style` attribute to any component
+    // snake-cased css properties become camelCased this this object
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.list}</li>
+    );
+  }
 }
 
-}
+
 // Updated TodoList to use TodoListItemsComponents
 var GroceryList = (props) => (
   <ul>
