cad39cf0dc0b5a7ce5263b5a084ad21aeed7ddc8
Khai Tran <kqtran917@yahoo.com>
2021-10-04 12:37:44 -0500

    Added example for handling event interaction

--- app.js
+++ app.js
@@ -25,14 +25,37 @@ var App = () => (
 
 // Version 2 using props
 
-var TodoList = (props) => (
-  <div><ul>
-    <li>{props.todos[0]}</li>
-    <li>{props.todos[1]}</li>
-    <li>{props.todos[2]}</li>
+// var TodoList = (props) => (
+//   <div><ul>
+//     <li>{props.todos[0]}</li>
+//     <li>{props.todos[1]}</li>
+//     <li>{props.todos[2]}</li>
+//     </ul>
+//     </div>
+// );
+
+// Handling Events
+
+var TodoList = (props) => {
+
+  // This function will be called when the first `<li>` below is clicked on
+  // Notice that event handling functions receive an `event` object
+  // We want to define it where it has access to `props`
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  // Because we used curly braces with this arrow function
+  // we have to write an explicit `return` statement
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.todos[0]}</li>
+      <li>{props.todos[1]}</li>
+      <li>{props.todos[2]}</li>
     </ul>
-    </div>
-);
+  );
+}
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
