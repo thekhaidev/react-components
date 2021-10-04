73dbd71faf2408d1b7ce9da92babfc2bf4555982
Khai Tran <kqtran917@yahoo.com>
2021-10-04 12:34:24 -0500

    Implemented new list using props

--- app.js
+++ app.js
@@ -2,24 +2,37 @@
 var App = () => (
   <div>
     <h2>My Todo List</h2>
-    <TodoList />
+    <TodoList todos={['Find drip', 'Find swag', 'Repeat']}/>
   </div>
 );
 
-var TodoList = () => (
+// Version 1 no Props
+// var TodoList = () => (
+//   <div><ul>
+//     <Drip />
+//     <Swag />
+//     </ul>
+//     </div>
+// );
+
+// var Drip = () => (
+//   <li>Drip</li>
+// );
+
+// var Swag = () => (
+//   <li>Swag</li>
+// );
+
+// Version 2 using props
+
+var TodoList = (props) => (
   <div><ul>
-    <Drip />
-    <Swag />
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
     </ul>
     </div>
 );
 
-var Drip = () => (
-  <li>Drip</li>
-);
-
-var Swag = () => (
-  <li>Swag</li>
-);
 
 ReactDOM.render(<App />, document.getElementById("app"));
